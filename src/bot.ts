b5df2aca74704f79a00d436f31c951deb15b19b1
fadzikri <109416385+fadzikri@users.noreply.github.com>
2022-12-12 07:54:35 +0700

    Add command

--- src/bot.ts
+++ src/bot.ts
@@ -4,9 +4,13 @@ import express from "express";
 const bot = new Bot(process.env.TELEGRAM_TOKEN || "");
 
 bot.command("start", (ctx) => ctx.reply(`Hello ${ctx.from?.username}`));
+bot.command("kernel", (ctx) => ctx.reply(`List kernel`));
+bot.command("last_kernel", (ctx) => ctx.reply(`6.11`));
 
 bot.api.setMyCommands([
   { command: "start", description: "Show start messages." },
+  { command: "kernel", description: "Display list all of kernels." },
+  { command: "last_kernel", description: "Last kernel published." },
 ]);
 
 if (process.env.NODE_ENV === "production") {
