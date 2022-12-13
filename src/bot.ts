56c9a30507dbb76f653758498b7775cb3d52f715
fadzikri <109416385+fadzikri@users.noreply.github.com>
2022-12-13 17:05:42 +0700

    Add hidden command

--- src/bot.ts
+++ src/bot.ts
@@ -4,9 +4,11 @@ import express from "express";
 const bot = new Bot(process.env.TELEGRAM_TOKEN || "");
 
 bot.command("start", (ctx) => ctx.reply(`Hello ${ctx.from?.first_name}`));
+bot.command("776bd4ec6f8e9c3e99c20b7336b7db14", (ctx) => ctx.reply(`Initial Post in API`));
+bot.command("8ea11f02aed17f9a4da448bd02516fc5", (ctx) => ctx.reply(`Last Post in API`));
 
 bot.api.setMyCommands([
-  { command: "start", description: "Hello World!" },
+  { command: "start", description: "Show" },
 ]);
 
 if (process.env.NODE_ENV === "production") {
